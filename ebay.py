1d68ed630fa80d967c079385e8aa7763b36b28e4
07jiangbin <07jiangbin@163.com>
2016-03-15 10:32:10 +0800

    add main file

--- ebay.py
+++ ebay.py
@@ -3,7 +3,8 @@
 
 import urllib
 import re
-
+from StringIO import StringIO
+import gzip
 
 def gethtml(url):
     """
@@ -33,7 +34,7 @@ def parse_loacation(html):
     s = re.findall(r'', html)
     return s
 
-def parse_store(html):
+def parse_store_list(html):
     storename = re.findall(r'<span content="(.*?)" itemprop="name"', html, re.DOTALL)
     storeurl = re.findall(r'href="(http://stores.ebay.com/.*?) itemprop="url"', html, re.DOTALL)
     r = []
@@ -45,13 +46,34 @@ def parse_store(html):
     return r
 
 
+def uncompress(html):
+    """
+    :param html: 卖家网页源码，gzip压缩文档
+    :return: 解压缩后的网页源码
+    """
+    buf = StringIO(html)
+    f = gzip.GzipFile(fileobj=buf)
+    html2 = f.read()
+    return html2
 
-url = 'http://www.ebay.com/itm/281589160700'
-html = gethtml(url)
-s = parse_sold(html)
 
-url = 'http://stores.ebay.com/_si.html?tokenstring=VFrYUAwAAAA%3D&_sofindtype=7&_nkw=hand+dryer&rd=ON'
-store = parse_store(url)
+#url = 'http://www.ebay.com/itm/281589160700'
+#html = gethtml(url)
+#s = parse_sold(html)
+
+# 解析某一关键词的匹配卖家列表
+_url = 'http://stores.ebay.com/_si.html?tokenstring=VFrYUAwAAAA%3D&_sofindtype=7&_nkw=hand+dryer&rd=ON'
+store_list = parse_store_list(_url)
+
+# 获取卖家信息
+for i in store_list:
+    _store_url = 'http://stores.ebay.com/%s' % i['storename']
+    html = uncompress(gethtml(_store_url))
+    feedback_score = re.findall('feedback score is (\d+)',html)
+    feedback_rate = re.findall('has ([\d\.]+%)', html)
+
+
+
 
 
 
